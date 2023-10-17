49e17f669d6be817c588c100a724c8c3398119fe
Rahul phatangare <rahulphatangare01@gmail.com>
2023-10-17 22:13:37 +0530

    update and delete task complete

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -13,6 +13,8 @@ export class TodoComponent implements OnInit {
   tasks :ITask [] = [];
   inprogress :ITask [] = [];
   done : ITask [] = [];
+  updateIndex!:any;
+  isEditEnabled:boolean = false;
 
   constructor(private fb: FormBuilder){}
 
@@ -28,6 +30,35 @@ export class TodoComponent implements OnInit {
       description:this.todoForm.value.item,
       done:false
     })
+    this.todoForm.reset();
+    console.log(this.tasks,'addd')
+  }
+
+  onEdit(item:ITask, i:number){
+    this.todoForm.controls['item'].setValue(item.description)
+    this.updateIndex = i;
+    this.isEditEnabled = true;
+
+  }
+  updateTask(){
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+
+  }
+
+
+
+  deleteTask(index:number){
+    this.tasks.splice(index,1)
+  }
+  deleteTaskInProgress(index:number){
+    this.inprogress .splice(index,1)
+  }
+  deleteDoneTask(index:number){
+    this.tasks.splice(index,1)
   }
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
